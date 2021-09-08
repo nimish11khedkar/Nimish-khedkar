dda9b069b7f663b1b240ad6299409a91b05cbedb
Nimish Khedkar <nimish.n.khedkar@gmail.com>
2021-09-08 15:19:12 +0530

    Name.java updated

--- Name.java
+++ Name.java
@@ -1,8 +1,12 @@
 public class Name {
     public static void main(String a[])
     {
-        int a=10;
-        int b=19;
-        System.out.println("Sum is : "+(a+b));
+            int a=10;
+            int b=19;
+            System.out.println("Sum is : "+(a+b));
+            /* Hi
+                Name is Nimish
+            */
+            System.out.println("I m trying to update my git");
     }
 }
